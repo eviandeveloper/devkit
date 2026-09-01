a4f8dfadb4d301c36a0c3810fb541f90ee252d1d
evian <alu0101395548@ull.edu.es>
2026-09-01 12:36:12 +0100

    Bump version to 0.7.6

--- cmake/devkitConfig.cmake
+++ cmake/devkitConfig.cmake
@@ -5,14 +5,13 @@ if(NOT TARGET fmt::fmt)
 endif()
 include("${CMAKE_CURRENT_LIST_DIR}/devkitTargets.cmake")
 
-# CREAR EL ALIAS PARA EL USUARIO FINAL
-# Si el target real existe y el alias no, lo creamos
+# Crear el alias para que el usuario pueda usar devkit::shapes
 if(TARGET devkit::devkit_shapes AND NOT TARGET devkit::shapes)
     add_library(devkit::shapes INTERFACE IMPORTED)
     target_link_libraries(devkit::shapes INTERFACE devkit::devkit_shapes)
 endif()
 
-# Hacer lo mismo para el main si quieres el nombre corto
+# Lo mismo para el main
 if(TARGET devkit::devkit_shapes_main AND NOT TARGET devkit::shapes_main)
     add_library(devkit::shapes_main INTERFACE IMPORTED)
     target_link_libraries(devkit::shapes_main INTERFACE devkit::devkit_shapes_main)
